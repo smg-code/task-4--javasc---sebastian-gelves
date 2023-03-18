49d8ab4e50be882457bab0de91b38a5a215c9163
Sebastian Local <imal_66@hotmail.com>
2023-03-18 13:56:48 -0300

    practico js 4 - descargo de la url los datos y  hago funcionar el home.js

--- details.js
+++ details.js
@@ -1,21 +1,47 @@
-const queryString = location.search  // la ruta - forma de generar el id para luego buscarlo
-                                     //<a href="./Ficha.html?id=${eventos[i]._id}" class="boton-ficha">Info</a>
-console.log(queryString)
-                                     
-const params = new URLSearchParams(queryString)   // instanciamos una url de una ruta
+let urlapi = ("https://mindhub-xj03.onrender.com/api/amazing")
+data = {}
+let  eventos = []
+let  fecha   = []
 
-const id = params.get("id") // obtenemos al id que esta en el home.js y es: ${eventos._id}
-                            // para el boton de info
-console.log(id)
-const eventoParaMostrarDetails = eventos.find(evento => eventos._id == id)
+//hago una funcion para traer los datos de la api
+//funcion asincronica
+async function traerdatosapi(){ 
+  try{
+    const response = await fetch(urlapi) //await espera la promesa y fetch trae los datos
+    const data = await response.json() //await espera la promesa y json convierte los datos en manejables
+    console.log(data)
+    let  eventos = data.events
+    let  fecha   = data.currentDate
 
-const div = document.querySelector("#fichas")
-div.innerHTML = ` <div class="card mb-3">
-                        <div class="card-body">
-                            <img class="card-img-top" src="${eventos[id-1].image}" alt="${eventos[id-1].name}">
-                            <h5 class="card-title">${eventos[id-1].name}</h5>
-                            <p class="card-text">${eventos[id-1].description}</p>
-                            <a href="./index.html" class="boton-ficha">Volver</a>
-                        </div>
-                    </div> `
+        const queryString = location.search  // la ruta - forma de generar el id para luego buscarlo
+                                            //<a href="./Ficha.html?id=${eventos[i]._id}" class="boton-ficha">Info</a>
+        console.log(queryString)
+                                            
+        const params = new URLSearchParams(queryString)   // instanciamos una url de una ruta
+
+        const id = params.get("id") // obtenemos al id que esta en el home.js y es: ${eventos._id}
+                                    // para el boton de info
+        console.log(id)
+        const eventoParaMostrarDetails = eventos.find(evento => eventos._id == id)
+
+        const div = document.querySelector("#fichas")
+        div.innerHTML = ` <div class="card mb-3">
+                                <div class="card-body">
+                                    <img class="card-img-top" src="${eventos[id-1].image}" alt="${eventos[id-1].name}">
+                                    <h5 class="card-title">${eventos[id-1].name}</h5>
+                                    <p class="card-text">${eventos[id-1].description}</p>
+                                    <a href="./index.html" class="boton-ficha">Volver</a>
+                                </div>
+                            </div> `
+   
+  }
+  catch(error) { //manejo del error
+    console.log(error)
+  }
+}
+
+traerdatosapi()
+
+
+
 
--- home.js
+++ home.js
@@ -1,9 +1,36 @@
-console.log([document])
+let urlapi = ("https://mindhub-xj03.onrender.com/api/amazing")
+let data = {}
+let  eventos = []
+let  fecha   = []
+
+//hago una funcion para traer los datos de la api
+//funcion asincronica
+async function traerdatosapi(){ 
+  try{
+    const response = await fetch(urlapi) //await espera la promesa y fetch trae los datos
+    const data = await response.json() //await espera la promesa y json convierte los datos en manejables
+    console.log(data)
+    eventos = data.events
+    fecha   = data.currentDate
+    
+    mostraCards(eventos);
+
+    Categorias = categoriasNoRep(eventos)
+    console.log("categorias",Categorias)
+
+    checkBoxesYSearch('#SelectroYBusqueda',Categorias)
+   
+  }
+  catch(error) { //manejo del error
+    console.log(error)
+  }
+}
+
+traerdatosapi()
 
 function mostraCards(eventos) {
     let body = ``;
     const tagToUpdate = document.getElementById("targetas");
-    console.log("tagToUpdate", tagToUpdate);
     for (let i = 0; i < eventos.length; i++) {
         body += `
                 <div class= "col-xs-12 col-sm-6 col-md-4 col-lg-3 card m-1" style="width: 18rem;"> 
@@ -24,10 +51,56 @@ function mostraCards(eventos) {
     tagToUpdate.innerHTML = body;
 }
 
-mostraCards(eventos);
+function categoriasNoRep(eventos) {
+  let Categorias = [] 
+  eventos.forEach(each => {                          
+      if ( ! Categorias.includes(each.category) ) {                                                  
+          Categorias.push(each.category)
+      }    
+  })
+  return Categorias
+  }
 
+function checkBoxesYSearch(id_etiquetas,category) { 
+    let barraSelectorYBusqueda = document.querySelector(id_etiquetas); 
+    console.log(barraSelectorYBusqueda)
+    category = category.map(each=> { 
+        return `
+        <fieldset>
+                <label class="contact-label" for="${each}">${each}</label>
+                <input onclick="captureData()" class="class_checks contact-input" 
+                type="checkbox" value="${each}" name="tipo" id="${each}">
+        </fieldset>
+        `;
+    })
+    category.push(`<input onkeyup="captureData()" id="ID_BUSQUEDA" 
+    class="contact-input" type="text" name="texto" placeholder="Busqueda">`)
+    barraSelectorYBusqueda.innerHTML = category.join('');
+}
 
+function captureData() {
+  let texto = document.getElementById('ID_BUSQUEDA').value.toLocaleLowerCase()
+  console.log(texto)
+  let checks = Array.from(document.querySelectorAll('.class_checks:checked')).map(each => each.value)
+  let filtro = eventos.filter(each => {   
+      return ((each.name.toLocaleLowerCase().includes(texto)) && 
+      (checks.length === 0 || checks.includes(each.category)))
+  })
+  if (filtro.length>0) {
+      mostraCards(filtro);
+  } else {
+      mostraCards(noEncontrada());
+  }
+}
 
+function noEncontrada() {
+  let nofound = [{_id: 0,
+          "image":"https://cdn.pixabay.com/photo/2016/05/30/14/23/detective-1424831_1280.png",
+          "name":"No se encontro evento",
+          "description":"haga una nueva busqueda.",
+  }]
+  return nofound
+  }
 
 
 
